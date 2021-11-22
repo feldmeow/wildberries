a8713c0c3fd7a5605aaefa63100271c0b9a1d264
Jauhen Feldman <feldmeow@gmail.com>
2021-11-22 18:33:14 +0300

    add view all function

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import cart from './modules/cart';
 import getGoods from './modules/getGoods';
 import search from './modules/search';
+import viewAll from './modules/viewAll';
 
 cart();
 getGoods();
 search();
+viewAll();
