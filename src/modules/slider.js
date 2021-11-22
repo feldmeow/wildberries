0700e021c6687f6bca8fae3103238388a42f4a49
Jauhen Feldman <feldmeow@gmail.com>
2021-11-23 00:06:46 +0300

    add swiper-slider

---
+++ src/modules/slider.js
@@ -0,0 +1,22 @@
+import Swiper, { Autoplay, Navigation, Pagination } from 'swiper';
+
+Swiper.use([Navigation, Pagination, Autoplay]);
+
+const slider = () => {
+	const swiper = new Swiper('.swiper-container', {
+		loop: true,
+		speed: 700,
+
+		autoplay: {
+			delay: 2500,
+			disableOnInteraction: false,
+		},
+
+		navigation: {
+			nextEl: '.slider-button-next',
+			prevEl: '.slider-button-prev',
+		},
+	});
+};
+
+export default slider;
